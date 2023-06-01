b2ee6a328b4d1aa618c306fcc9791ec82cd59ba5
Sakshi Mandhana <sakshimandhana12@gmail.com>
2023-06-01 20:48:59 +0530

    commit

--- app.py
+++ app.py
@@ -1,90 +1,55 @@
-from flask import Flask, render_template, request, send_from_directory,redirect
+from flask import Flask, render_template, request
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 from mlxtend.frequent_patterns import apriori, association_rules
-import io
-import chardet
+from plotly.offline import init_notebook_mode, iplot
+
+init_notebook_mode(connected=True)
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/')
 def index():
-    table_html = ''  # Initialize the table_html variable
-    data_file = None
-
-    if request.method == 'POST':
-        if 'data_file' not in request.files:
-            return render_template('index.html')
-
-        file = request.files['data_file']
-        if file.filename == '':
-            return render_template('index.html')
-
-        data = pd.read_csv(file, encoding='ansi')
-        data_file = file.filename
-
-        # Create the table figure
-        fig = go.Figure(data=[
-            go.Table(header=dict(values=list(data.columns), fill_color='paleturquoise', align='left'),
-                     cells=dict(values=[data['Invoice Date'], data['Invoice No.'], data['Product Name'], data['QTY'],
-                                        data['RATE']], fill_color='lavender', align='left')
-                     )])
-
-        # Specify the columns to be displayed
-        columns_to_display = ['Invoice Date', 'Invoice No.', 'Product Name', 'QTY', 'RATE']
-
-        # Remove columns that are not in the specified list
-        columns_to_remove = [col for col in data.columns if col not in columns_to_display]
-        fig.data[0].cells.values = [data[col] for col in columns_to_display]
-        fig.data[0].header.values = columns_to_display
-
-        # Convert the table figure to HTML
-        table_html = fig.to_html(full_html=False)
-
-        # Check the action requested by the submit button
-        submit_action = request.form['submit_action']
-        if submit_action == 'Generate Rules':
-            return redirect('/generate_rules')
-
-    return render_template('index.html', table_html=table_html, data_file=data_file)
+    return render_template('index.html')
 
 
-@app.route('/generate_rules', methods=['GET', 'POST'])
-def generate_rules_view():
-    data = request.files['data_file']
-    data = pd.read_csv(data, encoding='ansi')
+@app.route('/generate_rules', methods=['POST'])
+def generate_rules():
+    # Retrieve data from the uploaded file
+    file = request.files['data_file']
+    data = pd.read_csv(file, encoding='ansi')
 
+    # Data_preprocessing
+    data['Product Name'] = data['Product Name'].str.strip()  # removes spaces from beginning and end
+    data.dropna(axis=0, subset=['Invoice No.'], inplace=True)  # removes duplicate invoice
+    data['Invoice No.'] = data['Invoice No.'].astype('str')  # converting invoice number to be string
+    data = data[~data['Invoice No.'].str.contains('C')]  # remove the credit transactions
 
-    #Data_preprocessing
-    data['Product Name'] = data['Product Name'].str.strip() #removes spaces from beginning and end
-    data.dropna(axis=0, subset=['Invoice No.'], inplace=True) #removes duplicate invoice
-    data['Invoice No.'] = data['Invoice No.'].astype('str') #converting invoice number to be string
-    data = data[~data['Invoice No.'].str.contains('C')] #remove the credit transactions 
+    # grouping invoice no and products
+    mybasket = (data.groupby(['Invoice No.', 'Product Name'])['QTY'].sum().unstack().reset_index().fillna(0).set_index(
+        'Invoice No.'))
 
-    #grouping invoice no and products
-    mybasket = (data.groupby(['Invoice No.', 'Product Name'])['QTY'].sum().unstack().reset_index().fillna(0).set_index('Invoice No.'))
-    
     def my_encode_units(x):
-       if x <= 0:
-           return 0
-       if x >= 1:
-           return 1
+        if x <= 0:
+            return 0
+        if x >= 1:
+            return 1
 
     my_basket_sets = mybasket.applymap(my_encode_units)
-
 
     # Run the Apriori algorithm
     my_frequent_itemsets = apriori(my_basket_sets.astype('bool'), min_support=0.002, use_colnames=True)
     my_rules = association_rules(my_frequent_itemsets, metric="lift", min_threshold=1)
 
-
     modified_rules = my_rules.sort_values('lift', ascending=False)
-    modified_rules = modified_rules.drop_duplicates(subset='lift')
+    modified_rules
 
     # Generate the sunburst chart
-    fig = px.sunburst(modified_rules, path=['antecedents', 'consequents'], labels={'antecedents': 'Antecedents', 'consequents': 'Consequents'})
+    # Generate the sunburst chart
+    fig = px.sunburst(modified_rules, path=['antecedents', 'consequents'],
+                      labels={'antecedents': 'Antecedents', 'consequents': 'Consequents'})
 
     # Modify the labels
     fig.update_traces(textinfo='label+percent entry')
@@ -97,8 +62,8 @@ def generate_rules_view():
         return label
 
     # Convert frozenset objects to strings
-    modified_rules['antecedents'] = modified_rules['antecedents']#.apply(lambda x: ", ".join(x))
-    modified_rules['consequents'] = modified_rules['consequents']#.apply(lambda x: ", ".join(x))
+    modified_rules['antecedents'] = modified_rules['antecedents']  # .apply(lambda x: ", ".join(x))
+    modified_rules['consequents'] = modified_rules['consequents']  # .apply(lambda x: ", ".join(x))
 
     # Modify the labels in the DataFrame
     modified_rules['antecedents'] = modified_rules['antecedents'].apply(format_label)
@@ -112,26 +77,37 @@ def generate_rules_view():
 
     # Generate the second bar chart
     itemFrequency = data['Product Name'].value_counts().sort_values(ascending=False)
-    fig1 = px.bar(itemFrequency.head(25), title='25 Most Frequent Items', color=itemFrequency.head(25), color_continuous_scale=px.colors.sequential.Magenta)
-    fig1.update_layout(margin=dict(t=50, b=0, l=0, r=0), titlefont=dict(size=20), xaxis_tickangle=-45, plot_bgcolor='white', coloraxis_showscale=False)
+    fig1 = px.bar(itemFrequency.head(25),
+                  # title='25 Most Frequent Items',
+                  color=itemFrequency.head(25),
+                  color_continuous_scale=px.colors.sequential.Magenta)
+    fig1.update_layout(margin=dict(t=50, b=0, l=0, r=0), titlefont=dict(size=20), xaxis_tickangle=-45,
+                       plot_bgcolor='white', coloraxis_showscale=False)
     fig1.update_yaxes(showticklabels=False, title=' ')
     fig1.update_xaxes(title=' ')
-    fig1.update_traces(texttemplate='%{y}', textposition='outside', hovertemplate='<b>%{x}</b><br>No. of Transactions: %{y}')
+    fig1.update_traces(texttemplate='%{y}', textposition='outside',
+                       hovertemplate='<b>%{x}</b><br>No. of Transactions: %{y}')
+
 
     # Generate the third bar chart
-    mpd = data.groupby('Invoice Date')['Product Name'].count().sort_values(ascending=False)
-    fig2 = px.bar(mpd.head(25), title='Most Productive Day', color=mpd.head(25), color_continuous_scale=px.colors.sequential.Mint)
-    fig2.update_layout(margin=dict(t=50, b=0, l=0, r=0), titlefont=dict(size=20), xaxis_tickangle=-45, plot_bgcolor='white', coloraxis_showscale=False)
-    fig2.update_xaxes(title=' ')
-    fig2.update_yaxes(showticklabels=False, title=' ')
-    fig2.update_traces(texttemplate='%{y}', textposition='outside', hovertemplate='<b>%{x}</b><br>No. of Transactions: %{y}')
+    # line chart
+    # Group data by date and calculate the total number of transactions
+    transactions_by_date = data.groupby('Invoice Date')['Invoice No.'].count().reset_index()
 
+    # Create the line chart
+    fig2 = go.Figure(data=go.Scatter(x=transactions_by_date['Invoice Date'], y=transactions_by_date['Invoice No.'],
+                                     mode='lines+markers', line=dict(color='blue'), marker=dict(size=8),
+                                     name='Transactions'))
+    # Customize the chart layout
+    fig2.update_layout(title='Total Number of Transactions by Date', xaxis_title='Date',
+                       yaxis_title='Number of Transactions')
     # Set the chart layout
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
-    #Generate the fourth chart
+    # Generate the fourth chart
     product_rates = data.groupby('Product Name').sum().reset_index()
-    product_rates = product_rates.sort_values('RATE', ascending=False).head(15)  # Sort by RATE and select top 15 products
+    product_rates = product_rates.sort_values('RATE', ascending=False).head(
+        15)  # Sort by RATE and select top 15 products
 
     # Modify the text attribute to include properly formatted revenue values
     text_labels = product_rates['RATE'][::-1].apply(lambda x: '{:,.0f}'.format(x))
@@ -149,7 +125,7 @@ def generate_rules_view():
     ))
 
     fig3.update_layout(
-        title='Top 15 revenue generated products',
+        # title='Top 15 revenue generated products',
         xaxis=dict(range=[3400, 30000]),
         yaxis=dict(
             categoryorder='array',
@@ -170,11 +146,29 @@ def generate_rules_view():
             yanchor='middle',
             xshift=10  # Adjust the x-coordinate for spacing
         )
-        for x_val, y_val, text_label in zip(product_rates['RATE'][::-1], product_rates['Product Name'][::-1], text_labels)
+        for x_val, y_val, text_label in
+        zip(product_rates['RATE'][::-1], product_rates['Product Name'][::-1], text_labels)
     ]
 
     fig3.update_layout(annotations=annotations)
 
+    iplot(fig3)
+
+    # # Extract and modify the association rules table
+    # association_df = modified_rules[['antecedents', 'consequents', 'lift']]
+    # association_df['Item A'] = association_df['antecedents'].str.replace(',', '')
+    # association_df['Item B'] = association_df['consequents'].str.replace(',', '')
+    # association_df['Strength Category'] = association_df['lift'].apply(lambda
+    #                                                                        x: 'Strongly associated' if x > 20 else 'Moderately associated' if 10 <= x <= 20 else 'Mildly associated')
+    # strength_order = ['Strongly associated', 'Moderately associated', 'Mildly associated']
+    # association_df['Strength Category'] = pd.Categorical(association_df['Strength Category'], categories=strength_order,
+    #                                                      ordered=True)
+    # modified_association_df = association_df[['Item A', 'Item B', 'Strength Category']].sort_values('Strength Category')
+
+    # Set the chart layout
+    fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+
+    # Strongly associated table
     # Extract the desired columns from my_rules
     association_df = modified_rules[['antecedents', 'consequents', 'lift']]
 
@@ -187,7 +181,8 @@ def generate_rules_view():
     association_df['Item B'] = association_df['Item B'].str.replace(',', '')
 
     # Calculate the strength category based on the lift value
-    association_df['Strength Category'] = association_df['lift'].apply(lambda x: 'Strongly associated' if x > 20 else 'Moderately associated' if 10 <= x <= 20 else 'Mildly associated')
+    association_df['Strength Category'] = association_df['lift'].apply(lambda
+                                                                           x: 'Strongly associated' if x > 20 else 'Moderately associated' if 10 <= x <= 20 else 'Mildly associated')
 
     # Filter the association rules table for strongly associated items
     strongly_associated_df = association_df[association_df['Strength Category'] == 'Strongly associated']
@@ -196,10 +191,48 @@ def generate_rules_view():
     strongly_associated_df = strongly_associated_df[['Item A', 'Item B']]
 
     # Display the strongly associated items table with a title
-    print("Strongly Associated Items:")
     strongly_associated_df.to_string(index=False)
     strongly_associated_products = strongly_associated_df.values.tolist()
 
+    # Calculate the most productive days
+    mpd = data.groupby('Invoice Date')['Product Name'].count().sort_values(ascending=False).head(25)
+
+    # Filter the data for the most productive days
+    most_productive_days = data[data['Invoice Date'].isin(mpd.index)]
+
+    # Group by 'Invoice Date' and 'Product Name', count the occurrences
+    most_sold_products = most_productive_days.groupby(['Invoice Date', 'Product Name']).size().reset_index(name='Count')
+
+    # Sort the table by count in descending order
+    most_sold_products = most_sold_products.sort_values(by='Count', ascending=False)
+
+    # Display only the top 10 most sold products
+    top_10_most_sold_products = most_sold_products.head(18)
+
+    # Display the resulting table of top 10 most sold products on the most productive days
+
+    # Assuming you have the itemFrequency data as a pandas Series
+    itemFrequency = data['Product Name'].value_counts().sort_values(ascending=False)
+
+    # Get the top 10 most frequent items
+    top_items = itemFrequency.head(18)
+
+    # Create a DataFrame from the top_items data
+    table_data = pd.DataFrame({'Product Name': top_items.index, 'Total Sale(Qty)': top_items.values})
+
+    # Display the table without the index column
+    # table_data.to_string(index=False)
+
+    # Assuming you have the data DataFrame with columns 'Product Name' and 'RATE'
+    product_rates = data.groupby('Product Name').sum().reset_index()
+    product_rates = product_rates.sort_values('RATE', ascending=False).head(
+        18)  # Sort by RATE and select top 18 products
+
+    # Create the table with selected columns
+    table_data_1 = product_rates[['Product Name', 'RATE']].rename(columns={'RATE': 'Revenue'})
+
+    # Display the table without the index column
+    # table_data.to_string(index=False)
 
     # Save the chart as HTML
 
@@ -207,10 +240,16 @@ def generate_rules_view():
     chart_html1 = fig1.to_html(full_html=False)
     chart_html2 = fig2.to_html(full_html=False)
     chart_html3 = fig3.to_html(full_html=False)
-
     # Render the results template with the charts
-    return render_template('results.html', chart_html=chart_html, chart_html1=chart_html1, chart_html2=chart_html2, chart_html3=chart_html3,strongly_associated_products=strongly_associated_products)
+    return render_template('results.html', chart_html=fig.to_html(full_html=False),
+                           chart_html1=fig1.to_html(full_html=False), chart_html2=fig2.to_html(full_html=False),
+                           chart_html3=fig3.to_html(full_html=False),
+                           # modified_association_df=modified_association_df.to_html(index=False),
+                           strongly_associated_products = strongly_associated_products,
+                           top_10_most_sold_products=top_10_most_sold_products.to_html(index=False),
+                           table_data=table_data.to_html(index=False),
+                           table_data_1=table_data_1.to_html(index=False))
 
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(debug=False)
